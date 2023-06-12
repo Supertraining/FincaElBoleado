dd00402ab666337cd999e2d268ce72f30618e0e0
MatíasJMaranga <marangamatias@gmail.com>
2023-06-12 10:51:38 -0300

    stylesUpdated

--- assets/javascript/tienda.js
+++ assets/javascript/tienda.js
@@ -1,14 +1,5 @@
-let showCar = document.getElementById("collapseOne");
-let buttonToggle = document.getElementById("buttonToggle");
 
-buttonToggle.addEventListener("mouseover", function(){
-    buttonToggle.click()})
-
-    buttonToggle.addEventListener("mouseout", function(){
-    buttonToggle.click() 
-});
-
-let carrito = document.getElementById("carrito") //link carrito
+let carrito = document.getElementById("carrito") 
 
 let precio =Number(document.getElementById("precioIva").innerHTML)
 
