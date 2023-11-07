e1a74c38b6fda62e5c01aed2ea0eb77e4f578be8
MatíasJMaranga <marangamatias@gmail.com>
2023-11-06 21:25:39 -0300

    Styles_Header_Footer

--- assets/javascript/tienda.js
+++
@@ -1,8 +0,0 @@
-
-let carrito = document.getElementById("carrito") 
-
-let precio =Number(document.getElementById("precioIva").innerHTML)
-
-carrito.addEventListener("click", function() {
-    alert(`El precio de su vino es $ ${precio*1.21}`)
-})
